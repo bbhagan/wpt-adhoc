d77903f8a218d6a160652f59eecb21cb2d3ad476
bbhagan <bbhagan@gmail.com>
2019-11-01 16:52:38 -0700

    CSV of data works

--- routes/download/downloadCSV.js
+++ routes/download/downloadCSV.js
@@ -1,5 +1,7 @@
 import { Router as _Router } from "express";
 import { getTestSet } from "../../public/static/js/wptInterface";
+import { sortTestsByURL } from "../../public/static/js/sortTests";
+import { calcUOMPrecision } from "../../public/static/js/mathUtils";
 
 import stringify from "csv-stringify";
 require("dotenv").config();
@@ -11,16 +13,55 @@ router.post("/", async (req, res) => {
 	//console.log(`body ${JSON.stringify(req.body)}`);
 
 	try {
-		const tests = await getTestSet(req.body.testIds, {
-			SERVER_URL,
-			SERVER_PORT
+		const tests = sortTestsByURL(
+			await getTestSet(req.body.testIds, {
+				SERVER_URL,
+				SERVER_PORT
+			}),
+			req.body.sorting
+		);
+		const csvData = [];
+		tests.forEach(test => {
+			//Description header
+			csvData.push([
+				`Test Id: ${test.testId}, Test URL: ${test.data.url}, Location: ${test.data.location}`
+			]);
+			//Table header
+			csvData.push(
+				[" "].concat(
+					req.body.resultOptions.map(requestOption => {
+						return `${requestOption.name} (${requestOption.uom})`;
+					})
+				)
+			);
+			//Iterate over run data
+			test.data.runs.forEach((run, idx) => {
+				csvData.push(
+					[`Run ${idx + 1}`].concat(
+						req.body.resultOptions.map(requestOption => {
+							return calcUOMPrecision(
+								run.firstView[requestOption.wptField],
+								requestOption.uom,
+								requestOption.decimalPlacePrecision
+							);
+						})
+					)
+				);
+			});
+			//Average Line
+			csvData.push(
+				[`Avg.`].concat(
+					req.body.resultOptions.map(requestOption => {
+						return calcUOMPrecision(
+							test.data.average.firstView[requestOption.wptField],
+							requestOption.uom,
+							requestOption.decimalPlacePrecision
+						);
+					})
+				)
+			);
 		});
-		console.log();
-		const data = [
-			{ Blah: "blort", Something: "awesome" },
-			{ Blah: "foo", Something: "even better" },
-			{ Blah: "apple", Something: "orange" }
-		];
+		console.log(`csvData: ${JSON.stringify(csvData)}`);
 
 		res.setHeader("Content-Type", "text/csv");
 		res.setHeader(
@@ -30,7 +71,7 @@ router.post("/", async (req, res) => {
 		res.setHeader("Cache-Control", "no-cache");
 		res.setHeader("Pragma", "no-cache");
 
-		stringify(data, { header: true }).pipe(res);
+		stringify(csvData, {}).pipe(res);
 	} catch (e) {
 		console.log(e);
 	}
